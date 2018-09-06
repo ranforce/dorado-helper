4f068e07392999181953f7363e798fd81da744cb
hlx <405606225@qq.com>
2018-09-06 16:03:35 +0800

    it works

---
+++ spec/dorado-helper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DoradoHelperView from '../lib/dorado-helper-view';
+
+describe('DoradoHelperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
